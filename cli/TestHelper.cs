cb31fb4ca3e33e427eb2483b5841bb54138379cf
BitBridger <75743166+jaronimus@users.noreply.github.com>
2025-04-03 13:31:31 +0200

    Added 5Ch2

--- cli/TestHelper.cs
+++ cli/TestHelper.cs
@@ -136,7 +136,7 @@ internal static class TestHelper
 
 
         var ch51 = new Course("5Ch1", 5, ["Tsalastra"]);
-        var ch52 = new Course("5Ch2", 5, ["Richter"]);
+        var ch52 = new Course("5Ch2", 5, ["Richter", "Felix G", "Josef", "Muhammed", "Luka", "Linyi", "Annabelle", "Georgy", "Elin", "Jaron", "Mara", "Catelyn", "Felix H", "Tina", "Alexander"]);
         var ch31 = new Course("3ch1", 3, ["Schönborn"]);
         MustContainAll(ch51, ch52, ch31);
 
