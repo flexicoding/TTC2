b96aef0849fa85543c17dbd8ec9a6908969c1479
BitBridger <75743166+jaronimus@users.noreply.github.com>
2025-04-08 22:35:53 +0200

    Added 2gk4 to TestHelper.cs

--- cli/TestHelper.cs
+++ cli/TestHelper.cs
@@ -146,9 +146,10 @@ internal static class TestHelper
         var g23 = new Course("2g3", 2, ["Sarbacher"]);
         // MustContainAll(g51, g21, g22, g23);
 
-        var gk51 = new Course("5GK1", 5, ["Weinbrenner", "Lilly", "Lotta"]);
+        var gk51 = new Course("5GK1", 3, ["Weinbrenner", "Lilly", "Lotta"]);
         var gk21 = new Course("2gk1", 2, ["Akst", "Felix G", "Amelie", "Josef", "Muhammed", "Luka", "Linyi", "Annabelle", "Georgy", "Joshua", "Eliah", "Emma S", "Lorenz", "Mara", "William", "Catelyn", "Felix H", "Tina", "Hendrik", "Richard", "Onyx", "Alexander"]);
         var gk22 = new Course("2gk2", 2, ["Akst", "Nola", "Miriana", "Michael", "Luana", "Juliane", "Johanna", "Tabita", "Marie-M", "Katharina", "Moana", "Marie", "Emma W", "Lisa", "Fenja", "Helen", "Bruno", "Jonathan", "Matilda", "Violet", "Lea", "Clara"]);
+        var gk24 = new Course("2gk4", 2, ["Weinbrenner", "Lilly", "Lotta", "Annabel", "Anna", "Jaron"]);
         MustContainAll(gk51, gk21, gk22);
 
         var eth51 = new Course("5Eth1", 5, ["Egner", "Pia", "Amelie", "Michael", "Marie-M", "Lorenz", "Lisa", "Helen", "Hendrik", "Jonathan"]);
