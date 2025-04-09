6d2ec402d886a5990d19c5df791c0cfaab89aa97
Barion <ametrin.studios@gmail.com>
2025-04-09 10:14:37 +0200

    added Fenja to 2rel1

--- cli/TestHelper.cs
+++ cli/TestHelper.cs
@@ -120,76 +120,76 @@ internal static class TestHelper
     public static IEnumerable<Course> GetRealTestSet()
     {
         const int PERSONS = 52;
-        var m51 = new Course("5M1", 5, ["Fischer", "Amelie", "Annabelle", "Catelyn", "Clara", "Felix H", "Georgy", "Helen", "Hendrik", "Jaron", "Joshua", "Linyi", "Luana", "Marie", "Matilda", "Michael", "Onyx", "Pia", "Richard", "Lotta", "Tina", "Sofiia"]);
-        var m52 = new Course("5M2", 5, ["Maier", "Jonathan", "Alexander", "Felix G", "Josef", "Lorenz", "Luka", "Moana", "Muhammed", "Violet", "William", "Eliah", "Fenja", "Lea", "Mara", "Serhii", "Lennard"]);
-        var m31 = new Course("3m1", 3, ["Maier", "Anna", "Annabel", "Bruno", "Emma S", "Emma W", "Johanna", "Miriana", "Nola", "Elin", "Tabitha", "Juliane", "Katharina", "Lilly", "Marie-M", "Lisa"]);
+        var m51 = Course("5M1", 5, ["Fischer", "Amelie", "Annabelle", "Catelyn", "Clara", "Felix H", "Georgy", "Helen", "Hendrik", "Jaron", "Joshua", "Linyi", "Luana", "Marie", "Matilda", "Michael", "Onyx", "Pia", "Richard", "Lotta", "Tina", "Sofiia"]);
+        var m52 = Course("5M2", 5, ["Maier", "Jonathan", "Alexander", "Felix G", "Josef", "Lorenz", "Luka", "Moana", "Muhammed", "Violet", "William", "Eliah", "Fenja", "Lea", "Mara", "Serhii", "Lennard"]);
+        var m31 = Course("3m1", 3, ["Maier", "Anna", "Annabel", "Bruno", "Emma S", "Emma W", "Johanna", "Miriana", "Nola", "Elin", "Tabitha", "Juliane", "Katharina", "Lilly", "Marie-M", "Lisa"]);
         MustContain(PERSONS + 2, m51, m52, m31);
 
-        var d51 = new Course("5D1", 5, ["Schwamm", "Bruno", "Anna", "Catelyn", "Emma W", "Lotta", "Juliane", "Katharina", "Lilly", "Marie-M"]);
-        var d31 = new Course("3d1", 3, ["Hieber", "Amelie", "Annabel", "Felix G", "Helen", "Johanna", "Josef", "Joshua", "Linyi", "Lorenz", "Matilda", "Michael", "Miriana", "Moana", "Muhammed", "Lisa"]);
-        var d32 = new Course("3d2", 3, ["Droysen", "Jonathan", "Alexander", "Annabelle", "Clara", "Emma S", "Felix H", "Jaron", "Luana", "Luka", "Marie", "Elin", "Tina", "Eliah"]);
-        var d33 = new Course("3d3", 3, ["Knöpfler-Seitz", "Hendrik", "Sofiia", "Pia", "Richard", "Serhii", "Tabitha", "Violet", "William", "Fenja", "Mara", "Nola", "Onyx", "Georgy", "Lea", "Lennard"]);
+        var d51 = Course("5D1", 5, ["Schwamm", "Bruno", "Anna", "Catelyn", "Emma W", "Lotta", "Juliane", "Katharina", "Lilly", "Marie-M"]);
+        var d31 = Course("3d1", 3, ["Hieber", "Amelie", "Annabel", "Felix G", "Helen", "Johanna", "Josef", "Joshua", "Linyi", "Lorenz", "Matilda", "Michael", "Miriana", "Moana", "Muhammed", "Lisa"]);
+        var d32 = Course("3d2", 3, ["Droysen", "Jonathan", "Alexander", "Annabelle", "Clara", "Emma S", "Felix H", "Jaron", "Luana", "Luka", "Marie", "Elin", "Tina", "Eliah"]);
+        var d33 = Course("3d3", 3, ["Knöpfler-Seitz", "Hendrik", "Sofiia", "Pia", "Richard", "Serhii", "Tabitha", "Violet", "William", "Fenja", "Mara", "Nola", "Onyx", "Georgy", "Lea", "Lennard"]);
         MustContain(PERSONS + 4, d51, d31, d32, d33);
 
-        var py51 = new Course("5Ph1", 5, ["Brökelmann", "Jonathan", "Mara", "Fenja", "Luana", "Katharina", "Felix G", "Felix H", "Josef", "Alexander", "Joshua", "Tina", "Matilda", "Serhii", "Linyi"]);
-        var py31 = new Course("3ph1", 3, ["Wedemayer", "Jaron", "Richard", "Pia", "Helen", "Sofiia"]);
+        var py51 = Course("5Ph1", 5, ["Brökelmann", "Jonathan", "Mara", "Fenja", "Luana", "Katharina", "Felix G", "Felix H", "Josef", "Alexander", "Joshua", "Tina", "Matilda", "Serhii", "Linyi"]);
+        var py31 = Course("3ph1", 3, ["Wedemayer", "Jaron", "Richard", "Pia", "Helen", "Sofiia"]);
 
-        var ch51 = new Course("5Ch1", 5, ["Tsalastra", "Clara", "Hendrik", "Marie", "Onyx", "Pia", "Richard", "William", "Lennard"]);
-        var ch52 = new Course("5Ch2", 5, ["Richter", "Felix G", "Josef", "Muhammed", "Luka", "Linyi", "Annabelle", "Georgy", "Elin", "Jaron", "Mara", "Catelyn", "Felix H", "Tina", "Alexander"]);
-        var ch31 = new Course("3ch1", 3, ["Schönborn"]);
+        var ch51 = Course("5Ch1", 5, ["Tsalastra", "Clara", "Hendrik", "Marie", "Onyx", "Pia", "Richard", "William", "Lennard"]);
+        var ch52 = Course("5Ch2", 5, ["Richter", "Felix G", "Josef", "Muhammed", "Luka", "Linyi", "Annabelle", "Georgy", "Elin", "Jaron", "Mara", "Catelyn", "Felix H", "Tina", "Alexander"]);
+        var ch31 = Course("3ch1", 3, ["Schönborn"]);
 
-        var b51 = new Course("5B1", 5, ["Akst", "Johanna", "Georgy", "Luka", "Catelyn"]);
-        var b31 = new Course("3b1", 3, ["Akst"]);
+        var b51 = Course("5B1", 5, ["Akst", "Johanna", "Georgy", "Luka", "Catelyn"]);
+        var b31 = Course("3b1", 3, ["Akst"]);
 
-        var g51 = new Course("5G1", 3, ["Heese", "Bruno", "Emma W", "Violet"]);
-        var g21 = new Course("2g1", 2, ["Heese", "Bruno", "Emma W", "Violet", "Jonathan", "Lorenz", "Hendrik", "Amelie", "Michael", "Pia", "Marie-M", "Lisa", "Lennard"]);
-        var g22 = new Course("2g2", 2, ["Egner", "Felix G", "Josef", "Muhammed", "Luka", "Georgy", "Elin", "Serhii", "Jaron", "Lotta", "Marie", "Mara", "William", "Helen", "Anna", "Catelyn", "Tina", "Sofiia", "Richard", "Onyx", "Alexander", "Clara"]);
-        var g23 = new Course("2g3", 2, ["Sarbacher"]);
+        var g51 = Course("5G1", 3, ["Heese", "Bruno", "Emma W", "Violet"]);
+        var g21 = Course("2g1", 2, ["Heese", "Bruno", "Emma W", "Violet", "Jonathan", "Lorenz", "Hendrik", "Amelie", "Michael", "Pia", "Marie-M", "Lisa", "Lennard"]);
+        var g22 = Course("2g2", 2, ["Egner", "Felix G", "Josef", "Muhammed", "Luka", "Georgy", "Elin", "Serhii", "Jaron", "Lotta", "Marie", "Mara", "William", "Helen", "Anna", "Catelyn", "Tina", "Sofiia", "Richard", "Onyx", "Alexander", "Clara"]);
+        var g23 = Course("2g3", 2, ["Sarbacher"]);
         MustContain(PERSONS + 3, g21, g22, g23);
 
-        var gk51 = new Course("5GK1", 3, ["Weinbrenner", "Lilly", "Lotta"]);
-        var gk21 = new Course("2gk1", 2, ["Akst", "Felix G", "Amelie", "Josef", "Muhammed", "Luka", "Linyi", "Annabelle", "Georgy", "Joshua", "Eliah", "Emma S", "Lorenz", "Mara", "William", "Catelyn", "Felix H", "Tina", "Hendrik", "Richard", "Onyx", "Alexander", "Pia"]);
-        var gk22 = new Course("2gk2", 2, ["Akst", "Nola", "Miriana", "Michael", "Luana", "Juliane", "Johanna", "Tabitha", "Marie-M", "Katharina", "Moana", "Marie", "Emma W", "Lisa", "Fenja", "Helen", "Bruno", "Jonathan", "Matilda", "Violet", "Lea", "Clara"]);
-        var gk24 = new Course("2gk4", 2, ["Weinbrenner", "Lilly", "Lotta", "Annabel", "Anna", "Jaron"]);
+        var gk51 = Course("5GK1", 3, ["Weinbrenner", "Lilly", "Lotta"]);
+        var gk21 = Course("2gk1", 2, ["Akst", "Felix G", "Amelie", "Josef", "Muhammed", "Luka", "Linyi", "Annabelle", "Georgy", "Joshua", "Eliah", "Emma S", "Lorenz", "Mara", "William", "Catelyn", "Felix H", "Tina", "Hendrik", "Richard", "Onyx", "Alexander", "Pia"]);
+        var gk22 = Course("2gk2", 2, ["Akst", "Nola", "Miriana", "Michael", "Luana", "Juliane", "Johanna", "Tabitha", "Marie-M", "Katharina", "Moana", "Marie", "Emma W", "Lisa", "Fenja", "Helen", "Bruno", "Jonathan", "Matilda", "Violet", "Lea", "Clara"]);
+        var gk24 = Course("2gk4", 2, ["Weinbrenner", "Lilly", "Lotta", "Annabel", "Anna", "Jaron"]);
 
-        var eth51 = new Course("5Eth1", 5, ["Egner", "Pia", "Amelie", "Michael", "Marie-M", "Lorenz", "Lisa", "Helen", "Hendrik", "Jonathan"]);
-        var eth21 = new Course("2eth1", 2, ["Weißer", "Moana", "Elin", "Bruno", "Lennard", "Joshua", "Georgy", "Felix H", "Annabelle", "Nola", "Muhammed", "Luana", "Eliah", "Emma S", "Felix G"]);
-        var eth22 = new Course("2eth2", 2, ["Knöpfler-Seitz", "Josef", "Luka", "Linyi", "Juliane", "Serhii", "Mara", "Anna", "Tina", "Sofiia", "Violet", "Lea"]);
+        var eth51 = Course("5Eth1", 5, ["Egner", "Pia", "Amelie", "Michael", "Marie-M", "Lorenz", "Lisa", "Helen", "Hendrik", "Jonathan"]);
+        var eth21 = Course("2eth1", 2, ["Weißer", "Moana", "Elin", "Bruno", "Lennard", "Joshua", "Georgy", "Felix H", "Annabelle", "Nola", "Muhammed", "Luana", "Eliah", "Emma S", "Felix G"]);
+        var eth22 = Course("2eth2", 2, ["Knöpfler-Seitz", "Josef", "Luka", "Linyi", "Juliane", "Serhii", "Mara", "Anna", "Tina", "Sofiia", "Violet", "Lea"]);
 
-        var rel51 = new Course("5Rel1", 5, ["Droesch", "Johanna", "Tabitha"]);
-        var rel21 = new Course("2rel1", 2, ["Droesch", "Johanna", "Tabitha", "Emma W", "Catelyn", "Alexander", "Katharina", "Matilda", "Miriana"]);
-        var rel22 = new Course("2rel2", 2, ["Key-Häring", "Lilly", "Jaron", "Lotta", "Annabel", "Marie", "William", "Richard", "Onyx", "Clara"]);
+        var rel51 = Course("5Rel1", 5, ["Droesch", "Johanna", "Tabitha"]);
+        var rel21 = Course("2rel1", 2, ["Droesch", "Johanna", "Tabitha", "Emma W", "Catelyn", "Alexander", "Katharina", "Matilda", "Miriana", "Fenja"]);
+        var rel22 = Course("2rel2", 2, ["Key-Häring", "Lilly", "Jaron", "Lotta", "Annabel", "Marie", "William", "Richard", "Onyx", "Clara"]);
         MustContain(PERSONS + 5, eth51, eth21, eth22, rel21, rel22);
 
-        var wi51 = new Course("5Wi1", 5, ["Novak", "Elin", "Serhii", "Lennard", "Sofiia"]);
+        var wi51 = Course("5Wi1", 5, ["Novak", "Elin", "Serhii", "Lennard", "Sofiia"]);
         MustContain(PERSONS + 2, gk21, gk22, gk24, wi51);
 
-        var e51 = new Course("5E1", 5, ["Weber", "Amelie", "Anna", "Annabel", "Clara", "Eliah", "Emma S", "Helen", "Violet", "Jaron", "Juliane", "Katharina", "Michael", "Moana", "Lotta", "Sofiia", "Tabitha"]);
-        var e31 = new Course("3e1", 3, ["Benz", "Pia", "Felix G", "Muhammed", "Linyi", "Elin", "Luana", "Serhii", "Marie", "Lorenz", "Mara", "Bruno", "Felix H", "Tina", "Hendrik", "Richard", "Alexander", "Josef"]);
-        var e32 = new Course("3e2", 3, ["Kohler", "Nola", "Luka", "Miriana", "Annabelle", "Georgy", "Joshua", "Johanna", "Marie-M", "Emma W", "William", "Lisa", "Lennard", "Catelyn", "Onyx", "Jonathan", "Matilda", "Lea"]);
+        var e51 = Course("5E1", 5, ["Weber", "Amelie", "Anna", "Annabel", "Clara", "Eliah", "Emma S", "Helen", "Violet", "Jaron", "Juliane", "Katharina", "Michael", "Moana", "Lotta", "Sofiia", "Tabitha"]);
+        var e31 = Course("3e1", 3, ["Benz", "Pia", "Felix G", "Muhammed", "Linyi", "Elin", "Luana", "Serhii", "Marie", "Lorenz", "Mara", "Bruno", "Felix H", "Tina", "Hendrik", "Richard", "Alexander", "Josef"]);
+        var e32 = Course("3e2", 3, ["Kohler", "Nola", "Luka", "Miriana", "Annabelle", "Georgy", "Joshua", "Johanna", "Marie-M", "Emma W", "William", "Lisa", "Lennard", "Catelyn", "Onyx", "Jonathan", "Matilda", "Lea"]);
         MustContain(PERSONS + 3 - 2, e51, e31, e32); // - Lilly - Fenja
 
-        var l51 = new Course("5L1", 2, ["Kutscherauer", "Elin", "Johanna", "Marie", "Emma W", "Fenja"]);
-        var l31 = new Course("3l1", 3, ["Kutscherauer", "Miriana", "Elin", "Johanna", "Tabitha", "Marie", "Emma W", "Fenja", "Helen", "Jonathan", "Violet"]);
+        var l51 = Course("5L1", 2, ["Kutscherauer", "Elin", "Johanna", "Marie", "Emma W", "Fenja"]);
+        var l31 = Course("3l1", 3, ["Kutscherauer", "Miriana", "Elin", "Johanna", "Tabitha", "Marie", "Emma W", "Fenja", "Helen", "Jonathan", "Violet"]);
 
-        var bk51 = new Course("5BK1", 5, ["Losing", "Emma S", "Miriana", "Juliane"]);
-        var bk21 = new Course("2bk1", 2, ["Waidosch", "Jonathan", "Lorenz", "Bruno", "Nola", "Emma W", "Lennard", "Eliah", "Amelie", "Onyx", "Violet", "Lotta", "Sofiia"]);
-        var bk22 = new Course("2bk2", 2, ["Waidosch", "Jaron", "Annabelle", "Anna", "Josef", "Matilda", "Muhammed", "Katharina", "Mara"]);
+        var bk51 = Course("5BK1", 5, ["Losing", "Emma S", "Miriana", "Juliane"]);
+        var bk21 = Course("2bk1", 2, ["Waidosch", "Jonathan", "Lorenz", "Bruno", "Nola", "Emma W", "Lennard", "Eliah", "Amelie", "Onyx", "Violet", "Lotta", "Sofiia"]);
+        var bk22 = Course("2bk2", 2, ["Waidosch", "Jaron", "Annabelle", "Anna", "Josef", "Matilda", "Muhammed", "Katharina", "Mara"]);
         MustContain(21 + 1, bk21, bk22);
 
-        var mu51 = new Course("5Mu1", 5, ["Schäfer", "Joshua", "Lea", "Eliah", "Luana"]);
-        var mu21 = new Course("2mu1", 2, ["Schäfer", "Luka", "Linyi", "Lilly", "Annabelle", "Georgy", "Serhii", "Marie-M", "William", "Lisa", "Catelyn", "Tina", "Hendrik", "Richard"]);
-        var mu22 = new Course("2mu2", 2, ["Schäfer", "Felix H", "Alexander", "Clara", "Moana", "Helen", "Michael", "Fenja", "Annabel", "Marie", "Johanna", "Tabitha"]);
+        var mu51 = Course("5Mu1", 5, ["Schäfer", "Joshua", "Lea", "Eliah", "Luana"]);
+        var mu21 = Course("2mu1", 2, ["Schäfer", "Luka", "Linyi", "Lilly", "Annabelle", "Georgy", "Serhii", "Marie-M", "William", "Lisa", "Catelyn", "Tina", "Hendrik", "Richard"]);
+        var mu22 = Course("2mu2", 2, ["Schäfer", "Felix H", "Alexander", "Clara", "Moana", "Helen", "Michael", "Fenja", "Annabel", "Marie", "Johanna", "Tabitha"]);
 
-        var sp51 = new Course("5Sp1", 3, ["Engel", "Nola", "Richard", "Onyx", "William"]);
-        var sp21 = new Course("2sp1", 2, ["Weiß"]);
-        var sp22 = new Course("2sp2", 2, ["Engel", "Pia", "Nola", "Luka", "Miriana", "Linyi", "Annabelle", "Luana", "Emma S", "Marie-M", "Katharina", "Annabel", "Lorenz", "William", "Lisa", "Catelyn", "Felix H", "Hendrik", "Richard", "Onyx", "Jonathan"]);
-        var sp23 = new Course("2sp3", 2, ["Schwamm", "Felix G", "Lilly", "Elin", "Johanna", "Clara", "Serhii", "Tabitha", "Moana", "Mara", "Bruno", "Tina", "Sofiia", "Lea"]);
+        var sp51 = Course("5Sp1", 3, ["Engel", "Nola", "Richard", "Onyx", "William"]);
+        var sp21 = Course("2sp1", 2, ["Weiß"]);
+        var sp22 = Course("2sp2", 2, ["Engel", "Pia", "Nola", "Luka", "Miriana", "Linyi", "Annabelle", "Luana", "Emma S", "Marie-M", "Katharina", "Annabel", "Lorenz", "William", "Lisa", "Catelyn", "Felix H", "Hendrik", "Richard", "Onyx", "Jonathan"]);
+        var sp23 = Course("2sp3", 2, ["Schwamm", "Felix G", "Lilly", "Elin", "Johanna", "Clara", "Serhii", "Tabitha", "Moana", "Mara", "Bruno", "Tina", "Sofiia", "Lea"]);
         MustContain(PERSONS + 3, sp21, sp22, sp23);
 
-        var mva21 = new Course("2mva1", 2, ["Fischer", "Josef", "Muhammed", "Linyi", "Joshua", "Serhii", "Marie", "Fenja", "Felix H", "Tina", "Sofiia", "Matilda", "Alexander"]);
+        var mva21 = Course("2mva1", 2, ["Fischer", "Josef", "Muhammed", "Linyi", "Joshua", "Serhii", "Marie", "Fenja", "Felix H", "Tina", "Sofiia", "Matilda", "Alexander"]);
 
-        var inf21 = new Course("2inf1", 2, ["Häcker", "Jonathan", "Jaron", "Lennard", "Felix H", "Muhammed", "Emma S"]);
+        var inf21 = Course("2inf1", 2, ["Häcker", "Jonathan", "Jaron", "Lennard", "Felix H", "Muhammed", "Emma S"]);
 
         ImmutableArray<Course> courses = [
             m51, m52, m31,
@@ -229,7 +229,7 @@ internal static class TestHelper
             if (missing.Any())
             {
                 Console.WriteLine($"{string.Join(", ", courses.Select(c => c.Slug))} misses {string.Join(", ", missing)}");
-            } 
+            }
         }
 
         static void MustContain(int expected, params IEnumerable<Course> courses)
@@ -240,5 +240,8 @@ internal static class TestHelper
                 Console.WriteLine($"{real} instead of {expected} in {string.Join(", ", courses.Select(c => c.Slug))}");
             }
         }
+
+        static Course Course(string slug, int lpt, IEnumerable<string> people)
+            => new(slug, lpt, people.Select(s => new Person(s)).ToFrozenSet());
     }
 }
--- core/Course.cs
+++ core/Course.cs
@@ -1,7 +1,3 @@
 namespace TTC.Core;
 
-public sealed record Course(string Slug, int LessionsPerTurnus, FrozenSet<Person> People)
-{
-    public Course(string slug, int lpt, IEnumerable<string> people) 
-        : this(slug, lpt, people.Select(s => new Person(s)).ToFrozenSet()) { }
-}
+public sealed record Course(string Slug, int LessionsPerTurnus, FrozenSet<Person> People);
