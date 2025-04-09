96a621e855bc06187d20581051f0823a74fbd05f
Barion <ametrin.studios@gmail.com>
2025-04-09 17:13:11 +0200

    course equality based on slug

--- core/Course.cs
+++ core/Course.cs
@@ -1,3 +1,16 @@
 namespace TTC.Core;
 
-public sealed record Course(string Slug, int LessionsPerTurnus, FrozenSet<Person> People);
+public sealed record Course(string Slug, int LessionsPerTurnus, FrozenSet<Person> People) : IEquatable<Course>
+{
+    public bool Equals(Course? other)
+    {
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return other is not null && Slug == other.Slug;
+    }
+
+    public override int GetHashCode() => Slug.GetHashCode();
+}
